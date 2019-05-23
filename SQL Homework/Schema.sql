b3533de773ff5ed87e2360791c5a0a8347b96ee8
Veronique Singh <phamnique@gmail.com>
2019-05-23 18:25:48 -0400

    Update Schema.sql

--- SQL Homework/Schema.sql
+++ SQL Homework/Schema.sql
@@ -10,30 +10,33 @@ CREATE TABLE Dept_emp(
 	);
 CREATE TABLE Dept_manager(
 	Dept_no varchar(255),
-	Emp_no integer PRIMARY KEY,
+	Emp_no integer,
 	From_date date,
-	To_date date
+	To_date date,
+	PRIMARY KEY (Dept_no),
+	FOREIGN KEY (Emp_no) REFERENCES Dept_emp(Emp_no)
 );
 CREATE TABLE Employees(
-	Emp_no integer PRIMARY KEY,
+	Emp_no integer,
 	Birth_date date,
 	First_name varchar(255),
 	Last_name varchar(255),
 	Gender varchar(255),
-	Hire_date date
+	Hire_date date,
+	PRIMARY KEY (Emp_no)
 );
 CREATE TABLE Salaries (
-	Emp_no integer PRIMARY KEY,
+	Emp_no integer,
 	Salary integer,
 	From_date date,
-	To_date date
+	To_date date,
+	PRIMARY KEY (Emp_no)
 );
 CREATE TABLE Titles (
 	Emp_no integer,
 	Title varchar(255),
 	From_date date,
-	To_date date,
-	FOREIGN KEY (Emp_no) REFERENCES Employees(Emp_no)
+	To_date date
 );
 select * from departments;
 select * from dept_emp;
@@ -41,3 +44,4 @@ select * from dept_manager;
 select * from employees;
 select * from salaries;
 select * from titles;
+
